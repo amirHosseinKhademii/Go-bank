f54acfee69043e3d761f0ac8fcf85eea5cd89c8b
Amirhossein Khademi <sourena2575@gmail.com>
2026-05-17 20:20:43 +0200

    ADD LIST

--- api/account.go
+++ api/account.go
@@ -3,7 +3,6 @@ package api
 import (
 	repository "bank/db/sqlc"
 	"database/sql"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -47,8 +46,6 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 
-	log.Printf("Fetching account with ID: %d\n", req.ID) // Debugging log
-
 	account, err := server.store.GetAccount(ctx, req.ID)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -60,4 +57,28 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, account)
+}
+
+type ListAccountsRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req ListAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, repository.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
