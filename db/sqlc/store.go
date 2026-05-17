f1a781b8aec472c12a798316c4c1097555991338
Amirhossein Khademi <sourena2575@gmail.com>
2026-05-17 17:21:00 +0200

    Fix update

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -73,31 +73,19 @@ func (s *Stor) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTx
 			return err
 		}
 
-		//fmt.Println(txName, ": get account 1 for update")
-		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
-		if err != nil {
-			return err
-		}
-
 		//fmt.Println(txName, ": update account 1")
-		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
-			ID:      account1.ID,
-			Balance: account1.Balance - arg.Amount,
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
 		})
 		if err != nil {
 			return err
 		}
 
-		//fmt.Println(txName, ": get account 2 for update")
-		account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
-		if err != nil {
-			return err
-		}
-
 		//fmt.Println(txName, ": update account 2")
-		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
-			ID:      account2.ID,
-			Balance: account2.Balance + arg.Amount,
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
 		})
 		if err != nil {
 			return err
