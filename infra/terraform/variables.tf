6de6ffcd3d9167dd48181b958682e47967cff4c1
Amirhossein Khademi <sourena2575@gmail.com>
2026-05-19 20:12:48 +0200

    Update terraform

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -73,7 +73,7 @@ variable "az_count" {
 variable "node_instance_type" {
   description = "EC2 instance type for the managed node group."
   type        = string
-  default     = "t3.medium" 
+  default     = "t3.medium"
 }
 
 variable "node_desired_size" {
